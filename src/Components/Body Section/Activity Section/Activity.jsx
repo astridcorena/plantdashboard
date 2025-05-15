679265f37e0ed533f21fc44a5044fccd5b71247f
astrid corena <astrid.corena@gmail.com>
2025-05-14 23:12:41 -0600

    cambios para saber si funciona con netlify

--- src/Components/Body Section/Activity Section/Activity.jsx
+++ src/Components/Body Section/Activity Section/Activity.jsx
@@ -13,7 +13,7 @@ const Activity = () => {
   return (
     <div className='activitySection'>
       <div className="heading flex">
-        <h1>Resent Activity</h1>
+        <h1>Resent Activity.</h1>
         <button className='btn flex'>
           See All
           <BsArrowRightShort className='icon'/> 
